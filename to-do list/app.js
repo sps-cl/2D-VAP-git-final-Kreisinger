28704be5697e41e0998885c626969a394c38335c
Jindrichkreisinger <jindrich.kreisinger@student.sps-cl.cz>
2023-09-28 11:54:36 +0200

    To-Do list with remove function done

--- to-do list/app.js
+++ to-do list/app.js
@@ -1,5 +1,7 @@
 const inputBox =document.getElementById("input-box");
 const listContainer =document.getElementById("list-container");
+removeTodo();
+checking();
 
 function addTodo(){
     if(inputBox.value === ''){
@@ -8,12 +10,28 @@ function addTodo(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-    }
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }   
     inputBox.value = "";
 }
 
+function checking(){
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
     }
 }, false)
+}
+
+
+
+function removeTodo() {
+    listContainer.addEventListener("click",function(e){
+        if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+        }
+    }, false)
+}
+
